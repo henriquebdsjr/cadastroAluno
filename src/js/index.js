ea48fc15f3da33f767f88b91de911382f9a8c160
henriquebdsjr <henriquecasa1@hotmail.com>
2022-03-29 21:28:22 -0300

    criarCadastro2]

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+let totalCadastros = 0;
+let nomes = [];
+let telefones = [];
+let datas = [];
+let notas = [];
+
+function minhaFuncao() {
+    let nome = document.getElementById("nome").value;
+    let telefones = document.getElementById("telefone").value;
+    let datas = document.getElementById("data").value;
+    let notas = document.getElementById("nota").value;
+    nomes[totalCadastros] = nome;
+    telefones[totalCadastros] = telefone;
+    datas[totalCadastros] = data;
+    notas[totalCadastros] = nota;
+    document.getElementById
+}
+
